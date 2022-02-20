ad6a421fbab2389a87731a8077cb6ec160f902a8
Malgorzata Jaskiewicz <m.jaskiewicz@connexin.co.uk>
2022-02-20 17:19:51 +0000

    check

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
  
-import React, { useRef } from "react";
-import Carousel from "react-elastic-carousel";
+import React from "react";
 import { CategoryElectronics, CategoryJawelery, CategoryMensClothing, CategoryWomensClothing } from "../components/CategoryComponent";
 import Header from "../components/Header";
 import Slider from "../components/Slider";
